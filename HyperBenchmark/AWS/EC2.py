5f78d66e1a41a3780a57fdef4f3200d39b0c6c8b
Toby the potato <tobyclh@gmail.com>
2019-01-26 20:23:14 +0900

    add execution

--- HyperBenchmark/AWS/EC2.py
+++ HyperBenchmark/AWS/EC2.py
@@ -2,21 +2,26 @@ import boto3
 import paramiko
 import logging as log
 import watchtower
+import os
+from pathlib import Path
+
+
 logFormatter = log.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 
 
 class Instance:
-    def __init__(self, imageid='ami-02b15491d756c53d1'):
+    def __init__(self, imageid='ami-02b15491d756c53d1', private_key=None):
         self.logger = log.getLogger(type(self).__name__)
         consoleHandler = log.StreamHandler()
         consoleHandler.setFormatter(logFormatter)
-        ec2Handler = watchtower.CloudWatchLogHandler()
-        ec2Handler.setFormatter(logFormatter)
         self.logger.addHandler(consoleHandler)
-        self.logger.addHandler(ec2Handler)
         session = boto3.Session(region_name="us-west-2") #Oregon
         self.imageid = imageid
         self.ec2 = session.resource('ec2')
+        if private_key is None:
+            self.private_key = os.environ.get('ec2_key_path', '')
+        else:
+            self.private_key = private_key
         self.instance = None
         return
 
@@ -29,6 +34,14 @@ class Instance:
             InstanceType='t2.micro'#'p2.xlarge'
         )
         self.instanceid = instance_info['StartingInstances']['InstanceId']
+        self.logger = log.getLogger(f'{type(self).__name__}({self.instanceid})')
+        consoleHandler = log.StreamHandler()
+        consoleHandler.setFormatter(logFormatter)
+        self.logger.addHandler(consoleHandler)
+        #add 
+        ec2Handler = watchtower.CloudWatchLogHandler()
+        ec2Handler.setFormatter(logFormatter)
+        self.logger.addHandler(ec2Handler)
 
     def terminate(self):
         self.ec2.instances.filter(InstanceIds=[self.instanceid]).terminate()
@@ -40,17 +53,16 @@ class Instance:
         self.logger.error('instance terminated')
 
     def execue(self, command):
-        key = paramiko.RSAKey.from_private_key_file(path/to/mykey.pem)
+        key = paramiko.RSAKey.from_private_key_file(self.private_key)
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # Connect/ssh to an instance
         try:
             # Here 'ubuntu' is user name and 'instance_ip' is public IP of EC2
             client.connect(hostname=instance_ip, username="ubuntu", pkey=key)
 
             # Execute a command(cmd) after connecting/ssh to an instance
             stdin, stdout, stderr = client.exec_command(command)
-            print (stdout.read())
+            self.logger.info(stdout.read())
 
             # close the client connection once the job is done
             client.close()
